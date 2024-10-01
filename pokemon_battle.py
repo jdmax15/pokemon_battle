d2a3faf4cea296c902b5d50c8422fe9151a389ea
jdmax15 <joel.maxwell15@gmail.com>
2024-10-01 16:24:02 +0930

    made attempts at getting types worked into its own class variable

--- pokemon_battle.py
+++ pokemon_battle.py
@@ -7,14 +7,19 @@ class Pokemon:
         self.name = name
         self.data = self.get_data()
         self.stats = self.get_stats()
+        self.type = self.get_types()
+
+    def __str__(self):
+        print_info = f"This pokemon's name is {self.name}.\nType: \n"
+        return print_info
 
     def get_data(self):
-    url = f"https://pokeapi.co/api/v2/pokemon/{self.name}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        sys.exit("Error, pokemon not found")
+        url = f"https://pokeapi.co/api/v2/pokemon/{self.name}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            sys.exit("Error, pokemon not found")
 
     def get_stats(self):
         if self.data:
@@ -25,13 +30,23 @@ class Pokemon:
                 "weight": pokemon_data["weight"],
                 "id": pokemon_data["id"]
             }
-
+    # NEED TO FIX THIS MESS
+    def get_types(self):
+            pokemon_data = self.data
             types = pokemon_data.get("types", [])
             types_len = len(types)
 
             # Assigns the pokemon type, if there are 2 types they are concatenated.
-            if types_len > 0:
-                pokemon_info['type'] = types[0]['type']['name']
-            if types_len > 1:
-                pokemon_info['type'] = pokemon_info['type'] + "/" + types[1]['type']['name']
+            if types_len == 1:
+                return f"{types[0]['type']['name']}
+            if types_len == 2:
+                return f"{types[0]['type']} + / + {types[1]['type']['name']}"
 
+def main():
+    pokemon1 = Pokemon("pikachu")
+    print(pokemon1)
+    pokemon2 = Pokemon("pidgey")
+    print(pokemon2)
+
+if __name__ == "__main__":
+    main()
